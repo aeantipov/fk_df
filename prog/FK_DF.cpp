283d396a890e02934cc5eb44bb575da1a1edd0b3
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2012-11-17 19:39:28 +0100

    Recursive containers - almost done

--- prog/FK_DF.cpp
+++ prog/FK_DF.cpp
@@ -11,21 +11,39 @@ int main()
 {
     Log.setDebugging(true);
     std::cout << "Hi!" << std::endl;
+    typedef Grid1dObject<1,ComplexType,FMatsubaraGrid1d> GF;
     //Grid1d<RealType> a1;
     //std::function<RealType(const int &)> F1=[&](const int & n) {return PI/10*(2*n+1);};
     //std::cout<<F1(-100) << std::endl;
     //Grid1d<RealType> n1(-100,100,F1);
     //INFO(n1);
     FMatsubaraGrid1d n2(-10,10,10);
+    std::shared_ptr<FMatsubaraGrid1d> a = std::make_shared<FMatsubaraGrid1d>(n2);
     //INFO(n2);
     //INFO(n2[2]);
-    std::function<ComplexType(ComplexType)> F2=[](ComplexType x) {return x;};
+    std::function<ComplexType(ComplexType)> F2=[](ComplexType x) {return x*x;};
     //INFO(F2(3));
     ComplexType out = n2.integrate<std::function<ComplexType(ComplexType)> > (F2);
     INFO(out);
     
     INFO(std::get<1>(n2.findSmaller(FMatsubara(5,7))));
 
-    Grid1dObject<ComplexType,FMatsubaraGrid1d> D1(n2, F2);
-    INFO(D1);
+    auto a1 = std::make_tuple(n2,n2);
+    std::cout << "type check: " << std::is_same<FMatsubaraGrid1d, std::tuple_element<0,std::tuple<FMatsubaraGrid1d,FMatsubaraGrid1d> >::type >::value << std::endl;
+    static_assert(std::is_same<FMatsubaraGrid1d, std::tuple_element<0,std::tuple<FMatsubaraGrid1d,FMatsubaraGrid1d> >::type >::value,"!!!");
+
+    GF D1(n2);
+   // INFO(D1);
+   // INFO(D1[4]);
+    Grid1dObject<2,ComplexType,FMatsubaraGrid1d,FMatsubaraGrid1d> D2(a1);
+    //D2[0].set(F2);
+    //FMatsubaraGrid1d n3;
+    Eigen::VectorXf x (Eigen::VectorXf::Constant(2,3)); 
+    DEBUG(x);
+    std::tuple<int,double,int> t = std::make_tuple(1,2.3,2);
+    std::tuple<double,int> t2;
+    std::tuple<int> t2_2;
+    std::tuple_cat(t2_2, t2) = t;
+    DEBUG(std::get<0>(t2_2));
+    DEBUG(std::get<0>(t));
 }
--- src/GridObject.h
+++ src/GridObject.h
@@ -7,41 +7,108 @@
 
 namespace FK { 
 
-template<typename ValueType, class GridType>
-class Grid1dObject {
+template<unsigned int N, typename ValueType, typename GridTypeN, typename ...GridType> 
+class Grid1dObject : public Grid1dObject<N-1,ValueType,GridType...>
+{
+static_assert(N>1, "N=1 has a specialized variant");
 protected:
-    GridType _grid;
-    VectorType<ValueType> _vals;
+    std::shared_ptr<GridTypeN> _grid;
+    std::shared_ptr<VectorType<Grid1dObject<N-1,ValueType,GridType...> > > _vals;
+    Grid1dObject();
 public:
-    Grid1dObject(const GridType& grid);
-    Grid1dObject(const GridType& grid,  std::function<ValueType (decltype(grid[0]))> f);
-    void set(std::function<ValueType (decltype(_grid[0]))> f);
-    ValueType operator()(decltype(_grid[0]) in ) const;
-    ValueType operator[](unsigned int i) const;
-    template <typename ValType, class GridType2> friend std::ostream& operator<<(std::ostream& lhs, const Grid1dObject<ValType,GridType2> &in);
+//    Grid1dObject(std::shared_ptr<GridType> grid,  std::function<ValueType (decltype((*grid)[0]))> f);
+//    void set(std::function<ValueType (decltype((*_grid)[0]))> f);
+    Grid1dObject( std::tuple<GridTypeN, GridType...> in);
+//    ValueType operator()(decltype((*_grid)[0]) in ) const;
+//    ValueType& operator[](unsigned int i);
+//    template <unsigned int M, typename ValType, class GridType2> friend std::ostream& operator<<(std::ostream& lhs, const Grid1dObject<M,ValType,GridType2> &in);
 };
 
-template<typename ValueType, class GridType>
-Grid1dObject<ValueType,GridType>::Grid1dObject(const GridType& grid,  std::function<ValueType (decltype(grid[0]))> f):_grid(grid)
+template <typename ValueType, class GridType>
+class Grid1dObject<1,ValueType, GridType> 
 {
-    _vals.resize(grid.getSize());
+protected:
+    std::shared_ptr<GridType> _grid;
+    std::shared_ptr<VectorType<ValueType> > _vals;
+    Grid1dObject();
+public:
+    Grid1dObject(GridType grid);
+}; 
+
+//
+// N=1
+//
+
+template<typename ValueType, class GridType>
+Grid1dObject<1,ValueType,GridType>::Grid1dObject()
+{
+    DEBUG("N=1");
+    //_grid.reset(new GridType);
+    _vals.reset(new VectorType<ValueType>);
+}
+
+template<typename ValueType, class GridType>
+Grid1dObject<1,ValueType,GridType>::Grid1dObject(GridType grid):_grid(std::make_shared<GridType>(grid))
+{
+    DEBUG("Initializing from grid, N=1");
+    _vals.reset(new VectorType<ValueType>);
+}
+
+//
+// N!=1
+//
+
+template<unsigned int N,typename ValueType, typename GridTypeN, typename ...GridType> 
+Grid1dObject<N,ValueType,GridTypeN, GridType...>::Grid1dObject()
+{
+    std::cout << "Default: N = " << sizeof...(GridType) << std::endl;
+    //_grid.reset(new GridType);
+    //_vals.reset(new VectorType<ValueType>);
+}
+
+
+template<unsigned int N,typename ValueType, typename GridTypeN, typename ...GridType> 
+Grid1dObject<N,ValueType,GridTypeN, GridType...>::Grid1dObject( std::tuple<GridTypeN, GridType...> in)
+{
+    DEBUG("Grid constructor N = " << sizeof...(GridType)+1);
+    int tuple_size = sizeof...(GridType)+1;
+    //_vals.reset(new VectorType<Grid1dObject<ValueType,GridType...> >::constant(std::get<0>(in).getSize(),  ));
+}
+
+/*
+template<unsigned int N, typename ValueType, class GridType>
+Grid1dObject<N,ValueType,GridType>::Grid1dObject(std::shared_ptr<GridType> grid):_grid(grid)
+{
+    DEBUG("N=" << N);
+    _vals.reset(new VectorType<ValueType>(_grid->getSize()));
+}
+
+template<unsigned int N, typename ValueType, class GridType>
+ValueType& Grid1dObject<N,ValueType,GridType>::operator[](unsigned int i)
+{
+    return (*_vals)[i];
+}
+
+template<unsigned int N, typename ValueType, class GridType>
+Grid1dObject<N,ValueType,GridType>::Grid1dObject(std::shared_ptr<GridType> grid,  std::function<ValueType (decltype((*grid)[0]))> f):Grid1dObject<N,ValueType,GridType>(grid)
+{
     this->set(f);
 }
 
-template<typename ValueType, class GridType>
-void Grid1dObject<ValueType,GridType>::set(std::function<ValueType (decltype(_grid[0]))> f)
+template<unsigned int N, typename ValueType, class GridType>
+void Grid1dObject<N,ValueType,GridType>::set(std::function<ValueType (decltype((*_grid)[0]))> f)
 {
-    assert(_vals.size() == _grid.getSize());
-    for (unsigned int i=0; i<_vals.size(); ++i) _vals[i]=f(_grid[i]);
+    assert(_vals->size() == (*_grid).getSize());
+    for (unsigned int i=0; i<_vals->size(); ++i) (*_vals)[i]=f((*_grid)[i]);
 }
 
-template <typename ValType, class GridType2> 
-std::ostream& operator<<(std::ostream& lhs, const Grid1dObject<ValType,GridType2> &in)
+template <unsigned int M, typename ValType, class GridType2> 
+std::ostream& operator<<(std::ostream& lhs, const Grid1dObject<M,ValType,GridType2> &in)
 {
-    lhs << in._vals; 
+    lhs << *(in._vals); 
     return lhs;
 }
-
+*/
 }
 #endif // endif::ifndef ___FK_GRID_OBJECT_H___
 
