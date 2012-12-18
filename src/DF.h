5fe12e3260922d87a8e43631a1e6465e48557337
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2012-12-18 15:01:02 +0100

    DF_Ladder

---
+++ src/DF.h
@@ -0,0 +1,12 @@
+#ifndef __FK_DF_H__
+#define __FK_DF_H__
+
+namespace FK {
+
+template <class Solver>
+struct DFLadder {
+
+}
+
+} // end of namespace FK
+#endif // endif :: #ifndef __FK_DF_H__
---
+++ src/DF.hpp
@@ -0,0 +1,7 @@
+#ifndef __FK_DF_HPP__
+#define __FK_DF_HPP__
+
+namespace FK {
+
+} // end of namespace FK
+#endif // endif :: #ifndef __FK_DF_H__
