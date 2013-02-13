7feb811db66407c48aa812998577bc97b4954a49
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2013-02-14 00:34:24 +0100

    Cleaning

--- src/DF.cpp
+++ src/DF.cpp
@@ -138,56 +138,25 @@ typename DFLadder<D>::GLocalType DFLadder<D>::operator()()
             auto Wq_args_static = std::tuple_cat(std::make_tuple(0.0),q);
             GD_shift = GD.shift(Wq_args_static);
 
-            INFO_NONEWLINE("\tStatic contribution...");
             auto dual_bubble = Diagrams::getBubble(this->GD, Wq_args_static);
             auto dual_bubble_matrix = dual_bubble.getData().getAsDiagonalMatrix();
-            const auto FullStaticV4 = Diagrams::BS(dual_bubble_matrix, StaticV4, true, _eval_BS_SC, _n_BS_iter, _BSmix);
+            decltype(StaticV4) FullStaticV4;
+            if (_EvaluateStaticDiagrams) {
+                INFO_NONEWLINE("\tStatic contribution...");
+                FullStaticV4 = Diagrams::BS(dual_bubble_matrix, StaticV4, true, _eval_BS_SC, _n_BS_iter, _BSmix);
+            };
             //const auto FullStaticV42 = Diagrams::BS(dual_bubble_matrix, StaticV4, true, true, 1000, _BSmix);
 
-            //DEBUG((FullStaticV4 - FullStaticV42).norm());
-
-            INFO_NONEWLINE("\tDynamic contribution...");
-            /*
-            for (auto iW : _bGrid.getPoints()) {
-                INFO2("iW = " << iW);
-                auto Wq_args = std::tuple_cat(std::make_tuple(iW),q);
-                typename GLocalType::PointFunctionType VertexFillf = std::bind(
-                    &FKImpuritySolver::getVertex4<BMatsubaraGrid::point, FMatsubaraGrid::point, FMatsubaraGrid::point>, std::cref(_S), iW, std::placeholders::_1, std::placeholders::_1); 
-                typename GLocalType::FunctionType Vertexf = 
-                    std::bind(&FKImpuritySolver::getVertex4<ComplexType, ComplexType, ComplexType>, std::cref(_S), ComplexType(iW), std::placeholders::_1, std::placeholders::_1); 
-                DynVertex4.fill(VertexFillf);
-                DynVertex4._f = Vertexf;
-                // Bethe-Salpeter vertex
-                DualDynBubble = Diagrams::getBubble(GD, Wq_args);
-                FullDualDynVertex4 = Diagrams::BS(DualDynBubble, DynVertex4, true, _eval_BS_SC, (_n_BS_iter>0?_n_BS_iter-1:0), _BSmix);
-                // Sigma
-                auto GD_shift_dyn = GD.shift(Wq_args);
-                typename GKType::PointFunctionType SigmaF;
-                auto SigmaF2 = [&](wkPointTupleType in)->ComplexType { 
-                    auto w = std::get<0>(in);
-                    return DynVertex4(w)*DualDynBubble(w)*GD_shift(in)*FullDualDynVertex4(w);
-                    };
-                SigmaF = __fun_traits<typename GKType::PointFunctionType>::getFromTupleF(SigmaF2);
-                addSigma.fill(SigmaF);
-                addSigma*=T;
-                INFO3("Sigma contribution diff = " << addSigma.diff(addSigma*0));
-                SigmaD+=addSigma/totalqpts;
-                };
-            */
-
+            
             decltype(StaticVertex4) DualBubbleDynamic(StaticVertex4.getGrids());
             decltype(StaticVertex4)::PointFunctionType dbfill = [&](FMatsubaraGrid::point w1, FMatsubaraGrid::point w2){
                 return -T*(GD[size_t(w1)]*GD_shift[size_t(w2)]).sum()/RealType(totalqpts);
                 };
             decltype(StaticVertex4) DynamicFullVertex4(StaticVertex4.getGrids());
             if (_EvaluateDynamicDiagrams) { 
+                INFO_NONEWLINE("\tDynamic contribution...");
                 DualBubbleDynamic.fill(dbfill);
                 DynamicFullVertex4 = Diagrams::BS(DualBubbleDynamic, StaticVertex4*(-1.0), true, _eval_BS_SC,(_n_BS_iter>0?_n_BS_iter-1:0),_BSmix);
-                };
-            
-            //INFO_NONEWLINE("\tSchwinger-Dyson correction...");
-
-            if (_EvaluateDynamicDiagrams) {
                 DualBubbleDynamic*=(-1.0)*StaticVertex4*DynamicFullVertex4;
                 addSigma=0.0;
                 for (auto w : _fGrid.getPoints()) { 
@@ -330,5 +299,33 @@ template struct DFLadder<1>;
 template struct DFLadder<2>;  
 //template struct DFLadder<3>;  
 //template struct DFLadder<4>;  
+
+/*
+            for (auto iW : _bGrid.getPoints()) {
+                INFO2("iW = " << iW);
+                auto Wq_args = std::tuple_cat(std::make_tuple(iW),q);
+                typename GLocalType::PointFunctionType VertexFillf = std::bind(
+                    &FKImpuritySolver::getVertex4<BMatsubaraGrid::point, FMatsubaraGrid::point, FMatsubaraGrid::point>, std::cref(_S), iW, std::placeholders::_1, std::placeholders::_1); 
+                typename GLocalType::FunctionType Vertexf = 
+                    std::bind(&FKImpuritySolver::getVertex4<ComplexType, ComplexType, ComplexType>, std::cref(_S), ComplexType(iW), std::placeholders::_1, std::placeholders::_1); 
+                DynVertex4.fill(VertexFillf);
+                DynVertex4._f = Vertexf;
+                // Bethe-Salpeter vertex
+                DualDynBubble = Diagrams::getBubble(GD, Wq_args);
+                FullDualDynVertex4 = Diagrams::BS(DualDynBubble, DynVertex4, true, _eval_BS_SC, (_n_BS_iter>0?_n_BS_iter-1:0), _BSmix);
+                // Sigma
+                auto GD_shift_dyn = GD.shift(Wq_args);
+                typename GKType::PointFunctionType SigmaF;
+                auto SigmaF2 = [&](wkPointTupleType in)->ComplexType { 
+                    auto w = std::get<0>(in);
+                    return DynVertex4(w)*DualDynBubble(w)*GD_shift(in)*FullDualDynVertex4(w);
+                    };
+                SigmaF = __fun_traits<typename GKType::PointFunctionType>::getFromTupleF(SigmaF2);
+                addSigma.fill(SigmaF);
+                addSigma*=T;
+                INFO3("Sigma contribution diff = " << addSigma.diff(addSigma*0));
+                SigmaD+=addSigma/totalqpts;
+                };
+            */
 
 }; 
